8a6eb4d64ea87254a2d484b59dc4ba8697d943ad
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 23:10:35 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,16 @@
-print('hello')
-print('world')
-print('hello')
-print('hello')
+def uses_only(word, available):
+    for letter in word:
+      if letter not in available:
+        return False
+    
+    return True
+  
+  
+ def uses_only(word, available):
+     return set(word) <= set(available)
+  
+  
+  from collections import counter
+  count = Counter('parrot')
+  count
+  Counter({'r',:2,'t':1,'o':1,'p':1, 'a':1})
